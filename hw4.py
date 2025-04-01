f0db414d600358aa2b819b3c4b6e4c64cafe1b4b
tkddn2966 <116936914+tkddn2966@users.noreply.github.com>
2025-04-01 16:55:23 +0900

    hw4.py

--- hw4.py
+++ hw4.py
@@ -1,8 +1,10 @@
 def draw_line_string(n):
+    msg1 = " hello"+n
     msg2 = "Welcome to seoul."
-    nstr = len(n) if (len(n) > len(msg2)) else len(msg2)
+    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
     print("-"*nstr)
     print("Hello",n+",\n"+msg2)
     print("-"*nstr)
 
 draw_line_string(input("input his/her name : "))
+
