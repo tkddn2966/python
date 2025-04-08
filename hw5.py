7e63dcbb60c36bced65847721ff24bc0550cf7f7
tkddn2966 <116936914+tkddn2966@users.noreply.github.com>
2025-04-08 16:53:16 +0900

    Create hw5.py

---
+++ hw5.py
@@ -0,0 +1,32 @@
+def read_single_digit(n):
+    a = int(n[0])
+    return a
+
+def read_number(a):
+    if a == 1:
+        return "일"
+    elif a == 2:
+        return "이"
+    elif a == 3:
+        return "삼"
+    elif a == 4:
+        return "사"
+    elif a == 5:
+        return "오"
+    elif a == 6:
+        return "육"
+    elif a == 7:
+        return "칠"
+    elif a == 8:
+        return "팔"
+    elif a == 9:
+        return "구"
+    
+
+num = input("세 자리 정수 입력 :")
+res = read_single_digit(num[0])
+print(read_number(res), end =" ")
+res = read_single_digit(num[1])
+print(read_number(res), end =" ")
+res = read_single_digit(num[2])
+print(read_number(res), end =" ")
