c485366ff527892c86d934975b4e53fd672455a9
tkddn2966 <116936914+tkddn2966@users.noreply.github.com>
2025-04-29 16:38:41 +0900

    Create hw7.py

---
+++ hw7.py
@@ -0,0 +1,13 @@
+a = {}
+
+while True:
+    item = input("상품명? ")
+    if item == "":
+        break
+    count = int(input("수량은? "))
+    a[item] = count
+
+print(f"장바구니 보기 : {a}")
+
+b = input("장바구니에서 확인하고자 하는 상품은? ")
+print(f"{b}은(는) {a[b]}개 담겨 있습니다.")
