d9d9ec43100848673103faf4d3e21f44b510a645
tkddn2966 <116936914+tkddn2966@users.noreply.github.com>
2025-05-14 01:17:08 +0900

    Update hw9.py

--- hw9.py
+++ hw9.py
@@ -1,25 +1,45 @@
-class Rectangle:
-    def __init__(self, x1=0,y1=0,x2=0,y2=0):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
+
+class point:
+    def __init__(self, x=0,y=0):
+        self.__x = x
+        self.__y = y
 
     def show(self):
-        print(f"좌측상단 꼭지점이 ({self.__x1}, {self.__y1})이고 우측 하단 꼭지점이 ({self.__x2}, {self.__y2})인 사각형입니다.")
+        print(f"{self.__x}, {self.__y}")
+    
+    def set(self,x,y):
+        self.__x = x
+        self.__y = y
+
+    def get(self):
+        return (self.__x, self.__y)
+
+
+
+class Rectangle:
+    def __init__(self, x1=0,y1=0,x2=0,y2=0):
+        self.__lt = point(x1,y1)
+        self.__rb = point(x2,y2)
+        
+        
+
+    def show(self):
+        print(f"좌측상단 꼭지점이 {self.__lt.get()}이고 우측 하단 꼭지점이 {self.__rb.get()}인 사각형입니다.")
         
     def getWidth(self):
-        return self.__x2 - self.__x1
+        return self.__rb.get()[0] - self.__lt.get()[0]
     
     def getHeight(self):
-        return self.__y2 - self.__y1
+        return self.__rb.get()[1] - self.__lt.get()[1]
     
     def getArea(self):
-        return (self.__x2 - self.__x1) * (self.__y2 - self.__y1)
+        return (self.__rb.get()[0] - self.__lt.get()[0]) * (self.__rb.get()[1] - self.__lt.get()[1])
     
     def getPerimeter(self):
-        return ((self.__x2 - self.__x1) + (self.__y2 - self.__y1))*2
+        return (self.__rb.get()[0] - self.__lt.get()[0]) + (self.__rb.get()[1] - self.__lt.get()[1])*2
     
+
+
 
 r1 = Rectangle(5,5,20,10)
 a = r1.getArea()
@@ -27,3 +47,4 @@ p = r1.getPerimeter()
 
 r1.show()
 print(f"넓이는 {a}, 둘레는 {p}")
+
