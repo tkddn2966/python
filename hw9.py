cdb4bd05dce4e0fdddc26ba86a0ee221c36663e8
tkddn2966 <116936914+tkddn2966@users.noreply.github.com>
2025-05-13 17:29:42 +0900

    Create hw9.py

---
+++ hw9.py
@@ -0,0 +1,29 @@
+class Rectangle:
+    def __init__(self, x1=0,y1=0,x2=0,y2=0):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def show(self):
+        print(f"좌측상단 꼭지점이 ({self.__x1}, {self.__y1})이고 우측 하단 꼭지점이 ({self.__x2}, {self.__y2})인 사각형입니다.")
+        
+    def getWidth(self):
+        return self.__x2 - self.__x1
+    
+    def getHeight(self):
+        return self.__y2 - self.__y1
+    
+    def getArea(self):
+        return (self.__x2 - self.__x1) * (self.__y2 - self.__y1)
+    
+    def getPerimeter(self):
+        return ((self.__x2 - self.__x1) + (self.__y2 - self.__y1))*2
+    
+
+r1 = Rectangle(5,5,20,10)
+a = r1.getArea()
+p = r1.getPerimeter()
+
+r1.show()
+print(f"넓이는 {a}, 둘레는 {p}")
